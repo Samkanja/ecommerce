fdcddcafa70771f134bf8724dfcbf3b65f4ac959
Samkanja <samuelkanja4@gmail.com>
2023-02-07 21:31:48 -0800

    added view class

--- item/models.py
+++ item/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
@@ -11,3 +12,19 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Item(models.Model):
+    category = models.ForeignKey(Category,related_name='items',on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True,null=True)
+    price = models.FloatField()
+    image = models.ImageField(upload_to='item_images',blank=True,null=True)
+    is_sold = models.BooleanField(default=True)
+    created_by = models.ForeignKey(User, related_name='items',on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
